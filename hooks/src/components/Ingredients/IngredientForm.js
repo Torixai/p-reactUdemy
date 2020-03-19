310a1fb9b8a0ea74259fbdeac81eaf31673b87db
Kisato <kisato.sakurai@nijibox.co.jp>
2020-03-19 19:04:42 +0900

    hooks　途中

--- hooks/src/components/Ingredients/IngredientForm.js
+++ hooks/src/components/Ingredients/IngredientForm.js
@@ -4,16 +4,20 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  //!!! here state Always is an OBJECT
+  //!!! class components state Always is an OBJECT
   // usestate return
   // 1. current state snapshot
   // 2. function that allows you to update state
   // so... inputState[0] = current state snapshot
-  const inputState = useState({title: '', amount: ''});
+
+  // const [inputState, setInputState] = useState({title: '', amount: ''});
+
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({title:enteredTitle, amount: enteredAmount});
   };
 
   return (
@@ -22,11 +26,34 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({title: event.target.value})}/>
+            <input type="text" 
+            id="title" 
+            // value={inputState.title} 
+            value = {enteredTitle}
+            // onChange={ event => {
+            //         const newTitle = event.target.value;
+            //         setInputState(prevInputState => ({
+            //         title: newTitle, 
+            //         amount: prevInputState.amount
+            //       }))
+            // if we have two closure {} there, e.target will not be handed simulatenously so store valuable
+            // }}
+            onChange = {e=>{
+              setEnteredTitle(e.target.value);
+            }}
+              />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({amount: event.target.value})}/>
+            <input type="number" 
+            id="amount" 
+            value={enteredAmount} 
+            onChange = { e => {
+              setEnteredAmount(e.target.value);
+            }
+
+            }
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
--- hooks/src/components/Ingredients/Search.js
+++ hooks/src/components/Ingredients/Search.js
@@ -1,15 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  console.log(props);
+  // {onloadIngredients: f}
+  const { onloadIngredients } = props;
+  // extract one function inside { }
+  console.log(onloadIngredients);
+  const [enteredFilter, setEnteredFilter] = useState('');
+
+  //-------
+  // object destructuring
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+  let obj = {foo: '009', bar:'933'};
+  const { foo } = obj;
+  console.log(foo);
+  // = 009
+
+  let objct = [1];
+  const [bar] = objct;
+  console.log(bar);
+  // = 1
+
+  obj = {foo: 'changed'};
+  console.log(foo);
+  // = 009
+  //-------
+
+  useEffect(() => {
+    const query = enteredFilter.length === 0 
+    ? ''
+    : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    fetch('https://practice-react-ingredients.firebaseio.com/ingredients.json' + query).then(
+      response => response.json()
+    ).then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      // if you use this here, 
+      // every time parent component rerendered then 
+      // functions created again so
+      // this useeffect run again n again...
+      // so we use useCallback
+      onloadIngredients(loadedIngredients);
+    });
+  }, [enteredFilter, onloadIngredients]);
+  // if [enteredFilter, props.onLoadIngredients] then re-render every time
+  // all new ingredients added
+  // const {xxxxx} = props; is Object restructuring
+  // right side stored as separate variable
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text" value={enteredFilter} 
+          onChange={e => {setEnteredFilter(e.target.value);
+          }}/>
         </div>
       </Card>
     </section>
